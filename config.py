c65606e423be87a747717690fb58cd4f9c39014c
Stanislava <pronto300@mail.ru>
2023-05-03 16:16:20 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '6240659554:AAF2S8zPUUuKueYPpUROYCQi1jI4SRlIoZY'
+
+keys = {
+    'доллар': 'USD',
+    'рубль': 'RUB',
+    'евро': 'EUR'
+}
---
+++ extensions.py
@@ -0,0 +1,31 @@
+import json
+import requests
+from config import keys
+
+class CryptoConverter():
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+        try:
+            amount = float(amount)
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+        if amount <= 0:
+            raise ConvertionException(f'Невозможно конвертировать количество валюты меньше или равное 0')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
+
+class ConvertionException(Exception):
+    pass
